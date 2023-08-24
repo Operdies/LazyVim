fd0bbb2a380918c6704869af92ee56869a17a711
alex <alexnlarsen@gmail.com>
2023-08-24 17:34:03 +0200

    add qmk

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -137,5 +137,19 @@ return {
         desc = "Tmux Down",
       },
     },
+    "codethread/qmk.nvim",
+    config = function()
+      ---@type qmk.UserConfig
+      local conf = {
+        name = "LAYOUT_first",
+        layout = {
+          "x x x x x _ _ _ x x x x x",
+          "x x x x x _ _ _ x x x x x",
+          "x x x x x x _ x x x x x x",
+          "x x x x x x _ x x x x x x",
+        },
+      }
+      require("qmk").setup(conf)
+    end,
   },
 }
